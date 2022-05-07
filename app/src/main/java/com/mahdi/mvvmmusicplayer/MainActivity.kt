52a6ce9b09d0d602d7a672f7cd1f70f746e88111
mhd.kanafchian <nrdc@321>
2022-05-07 11:58:24 +0430

    Initial commit

---
+++ app/src/main/java/com/mahdi/mvvmmusicplayer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mahdi.mvvmmusicplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
