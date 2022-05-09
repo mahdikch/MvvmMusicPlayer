41435acc472d6a385229a9a5b579a6cb546b9369
mhd.kanafchian <nrdc@321>
2022-05-09 09:25:28 +0430

    first commit

---
+++ app/src/main/java/com/mahdi/mvvmmusicplayer/MusicPlayerApplication.kt
@@ -0,0 +1,8 @@
+package com.mahdi.mvvmmusicplayer
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MusicPlayerApplication: Application()
+
---
+++ app/src/main/java/com/mahdi/mvvmmusicplayer/other/constants.kt
@@ -0,0 +1,7 @@
+package com.mahdi.mvvmMusicPlayer.other
+
+object constants {
+    const val SONG_COLLECTION="songs"
+    const val NOTIFICATION_CHANNEL_ID="music"
+    const val NOTIFICATION_ID=1
+}
